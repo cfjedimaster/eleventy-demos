78da2df879dfdf39287d5e8bd0e8afa6cde236c9
Raymond Camden <raymondcamden@gmail.com>
2020-07-04 18:03:04 -0500

    testing

---
+++ weather/_data/weather.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+
+// used to auth with HERE API
+const HERE_KEY = 'c1LJuR0Bl2y02PefaQ2d8PvPnBKEN8KdhAOFYR_Bgmw';
+// used for location of the market
+const LOC = 'Lafayette, LA';
+
+module.exports = async function() {
+	let url = `https://weather.ls.hereapi.com/weather/1.0/report.json?apiKey=${HERE_KEY}&product=observation&name=${encodeURIComponent(LOC)}&metric=false`;
+	console.log(url);
+	let resp = await fetch(url);
+	let data = await resp.json();
+	let report = data.observations.location[0].observation[0];
+	console.log(report);
+
+	// Add a simplification
+	report.rainWarning = (report.precipitation12H !== '*' && report.precipitation12H > 0.02);
+
+	return report;
+
+}
+
+// https://developer.here.com/documentation/destination-weather/dev_guide/topics/resource-type-weather-items.html

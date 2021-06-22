cb03f967907421bfb69327f88a8b04df3581caa7
Raymond Camden <raymondcamden@gmail.com>
2021-06-22 18:42:59 -0500

    better version

--- gourls/.eleventy.js
+++ gourls/.eleventy.js
@@ -1,39 +1,12 @@
 const fs = require('fs');
 
 module.exports = function(eleventyConfig) {
-	let allPages;
 
-	eleventyConfig.addPassthroughCopy("./_redirects");
 
-	eleventyConfig.addCollection("allMyContent", function(collectionApi) {
-		// copy out so I can usee it in afterBuild
-		allPages = collectionApi.getAll();
-		// allMyContent is basically unused
-    	return collectionApi.getAll();
-	});
-
-	eleventyConfig.on('afterBuild', () => {
-		console.log('afterBuild');
-		// read in _redirects, if it exists, if not, we end early
-		let redir = './_site/_redirects';
-
-		if(!fs.existsSync(redir)) return;
-		let contents = fs.readFileSync(redir, 'utf8');
-		let newContent = '\n';
-
-		for(let page of allPages) {
-			if(page.data.go) {
-				newContent += `
-/go/${page.data.go}	${page.url}`;
-			}
-		}
-
-		if(newContent !== '\n') {
-			console.log('Adding custom GO urls ');
-		}
-		contents += newContent;
-		fs.writeFileSync(redir, contents);
-		
+	eleventyConfig.addCollection("goPages", function(collectionApi) {
+    	return collectionApi.getAll().filter(p => {
+			if(p.data.go) return true;
+		});
 	});
 
 };
--- gourls/posts/alpha.md
+++ gourls/posts/alpha.md
@@ -1,5 +1,6 @@
 ---
 title: Alpha Post
+go: apple
 ---
 
 This is Alpha.

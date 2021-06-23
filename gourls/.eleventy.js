e4a857a76d9505b8f7bbca07141b839ef4966aec
Raymond Camden <raymondcamden@gmail.com>
2021-06-22 19:09:57 -0500

    oops

--- gourls/.eleventy.js
+++ gourls/.eleventy.js
@@ -6,6 +6,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addCollection("goPages", function(collectionApi) {
     	return collectionApi.getAll().filter(p => {
 			if(p.data.go) return true;
+			return false;
 		});
 	});
 

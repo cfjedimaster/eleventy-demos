26b1283f6948b7e3be57852d49fa8fcfc936861f
Raymond Camden <raymondcamden@gmail.com>
2021-06-22 19:10:41 -0500

    arrow ftw

--- gourls/.eleventy.js
+++ gourls/.eleventy.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = function(eleventyConfig) {
 
 
-	eleventyConfig.addCollection("goPages", function(collectionApi) {
+	eleventyConfig.addCollection("goPages", collectionApi => {
     	return collectionApi.getAll().filter(p => {
 			if(p.data.go) return true;
 			return false;

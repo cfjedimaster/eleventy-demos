002f6a9c83d84297f220ce632e775edfb1752724
Raymond Camden <raymondcamden@gmail.com>
2021-07-30 13:57:12 -0500

    dumb demo

--- fetchshortcode/.eleventy.js
+++ fetchshortcode/.eleventy.js
@@ -1,13 +1,24 @@
 const fetch = require('node-fetch');
 
 module.exports = function(eleventyConfig) {
+;
 
-	eleventyConfig.addFilter("fetch", async function(url) {
-		console.log('called fetch with '+url);
+	/*
+	eleventyConfig.addPairedShortcode("fetch2", async function(content, url) {
+		console.log('called fetch2 with '+url);
 		let resp = await fetch(url);
 		let data = await resp.json();
 		console.log('data', data.results.length);
-		return data;
-  	});
+		this.page.fetchdata = data;
+		return '';
+	});
+	*/
+	
+	eleventyConfig.addShortcode("fetch", async function(url, name, filter) {
+		let resp = await fetch(url);
+		let data = await resp.json();
+		if(filter) data = data[filter];
+		this.page[name] = data;
+	});
 
 }

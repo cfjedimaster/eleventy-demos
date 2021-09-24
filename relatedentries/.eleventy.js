e2b37c7b197154cda48637b063096993c54832eb
Raymond Camden <raymondcamden@gmail.com>
2021-09-24 14:38:21 -0500

    new demo

---
+++ relatedentries/.eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig) {
+
+	const english = new Intl.DateTimeFormat('en');
+
+	eleventyConfig.addFilter("dtFormat", function(date) {
+		return english.format(date);
+	});
+
+	eleventyConfig.addFilter("getRelated", function(relatedPosts, all) {
+		console.log('getRelated', relatedPosts);
+		/*
+		relatedPosts is an array of filePathStems, return an array
+		of page obs that match
+		*/
+		
+		return all.filter(p => {
+			return relatedPosts.includes(p.filePathStem);
+		});
+		
+	});
+
+};
---
+++ relatedentries/posts/gamma.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Gamma Post
+tags: posts
+date: 2020-10-10 12:00:00
+related:
+    - /posts/alpha
+    - /posts/beta
+---
+
+This is the Gamma post.
+

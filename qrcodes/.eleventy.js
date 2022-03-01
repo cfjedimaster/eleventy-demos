4e5fc93962f1609b6ec00c8572a2fa9b9648608b
Raymond Camden <raymondcamden@gmail.com>
2022-03-01 08:55:43 -0600

    nunjucks workaround

--- qrcodes/.eleventy.js
+++ qrcodes/.eleventy.js
@@ -8,5 +8,14 @@ module.exports = function(eleventyConfig) {
 
 	});
 
+	/*
+	Based on an email from Will, I discovered that nunjucks can't use an async *global* filter. If
+	you define one using the code below, it works just fine. Filing a bug for this.
+	*/
+	eleventyConfig.addNunjucksAsyncFilter("qrcode2", async function(value, callback) {
+		let result = await qrCode.toDataURL(value);
+		callback(null,result);
+	});
+
 };
 

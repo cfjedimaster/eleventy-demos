493f046e498b805dcbd2b8b7bb751605bd2995a8
Raymond Camden <raymondcamden@gmail.com>
2022-06-18 13:54:54 -0500

    testing 3

--- quiz/src/_data/quizzes.js
+++ quiz/src/_data/quizzes.js
@@ -12,6 +12,8 @@ module.exports = function() {
 	console.log('./',fs.readdirSync('./'));
 	console.log('../',fs.readdirSync('../'));
 	console.log('../../',fs.readdirSync('../../'));
+	console.log('../../../',fs.readdirSync('../../../'));
+	console.log('../../../../',fs.readdirSync('../../../../'));
 
 
 	let files = fs.readdirSync(inputDir);

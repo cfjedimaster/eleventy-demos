6444bbd9264a56a0ba37bff111095b0ca1e1a954
Raymond Camden <raymondcamden@gmail.com>
2022-07-22 15:44:03 -0500

    misc

--- autozip/.eleventy.js
+++ autozip/.eleventy.js
@@ -1,7 +1,20 @@
+const AdmZip = require('adm-zip');
+const glob = require('glob-promise');
+
 module.exports = function(eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy('src/images');
 	
+	eleventyConfig.on('eleventy.after', async () => {
+		console.log('after build');
+		let catpics = await glob('./src/images/cats/*.jpg');
+		console.log(catpics);
+		let zip = new AdmZip();
+		catpics.forEach(c => zip.addLocalFile(c));
+		zip.writeZip('_site/catpics.zip');
+
+
+	});
 
 	return {
 		dir: {

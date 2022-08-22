b4c81bb48e419abaf367fee1129091cbe79a5e42
Raymond Camden <raymondcamden@gmail.com>
2022-08-22 15:11:57 -0500

    new demo

---
+++ sharelinks/.eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+
+	const english = new Intl.DateTimeFormat('en');
+
+	eleventyConfig.addFilter("dtFormat", function(date) {
+		return english.format(date);
+	});
+
+	eleventyConfig.addGlobalData("rootURL", "https://www.raymondcamden.com");
+};
---
+++ sharelinks/posts/alpha.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: Alpha Post
+tags: posts
+date: 2020-10-01 12:00:00
+---
+
+This is the Alpha post.

e0c20bb4cb7d4a5131f3918a5dd8d28522d31599
Raymond Camden <raymondcamden@gmail.com>
2022-10-16 13:44:35 -0500

    new demo

---
+++ webctest1/.eleventy.js
@@ -0,0 +1,7 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(pluginWebc, {
+		components:"_includes/components/**/*.webc"
+	});
+};
